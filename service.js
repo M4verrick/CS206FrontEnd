c2e3011320a2eaf3bae28395b9603465d2034f0f
M4verrick <mxxue.2022@scis.smu.edu.sg>
2024-03-21 17:12:12 +0800

    added API calls to service.js

--- service.js
+++ service.js
@@ -1,120 +1,171 @@
 import axios from "axios";
 
 //need change to ip address
-const API_URL = "http://localhost:8080/api/v1/";
-axios.defaults.withCredentials = true
+const API_URL = "http://172.20.10.3:8080/api/v1/";
+axios.defaults.withCredentials = true;
+
+// register new user
+const signUp = (userName, userEmail, userPassword) => {
+  return axios
+    .post(
+      API_URL + `user/${userName}/${userEmail}/${userPassword}/signUp`,
+      {
+        userName,
+        userEmail,
+        userPassword,
+      },
+      { withCredentials: true }
+    )
+    .then((response) => {
+      const data = response.data;
+      console.log("Signup successful:", data);
+      // process the success response
+    })
+    .catch((error) => {
+      console.error("Error during signup:", error);
+      throw error;
+    });
+};
+
+// @PostMapping("{userEmail}/{userPassword}/login")
+const login = (userEmail, userPassword) => {
+  return axios
+    .post(API_URL + `user/${userEmail}/${userPassword}/login`)
+    .then((response) => {
+      const isAuthenticated = response.data;
+      // isAuthenticated will be true or false based on your backend response
+      return isAuthenticated;
+    })
+    .catch((error) => {
+      console.error("Error during login:", error);
+      // Handle errors appropriately
+      throw error;
+    });
+};
 
 // @PostMapping("/{teamName}/{teamEmails}/createTeam")
 // public ResponseEntity<Team> createTeam
 const createTeam = (teamName, teamEmails) => {
-    return axios.post
-    (API_URL + `team/${teamName}/${teamEmails}/createTeam`, {
-        teamName, teamEmails
-    },
-        { withCredentials: true,}
+  return axios
+    .post(
+      API_URL + `team/${teamName}/${teamEmails}/createTeam`,
+      {
+        teamName,
+        teamEmails,
+      },
+      { withCredentials: true }
     )
-    .then((response) => { 
-        const inSet = response.data;
-        console.log(inSet)
+    .then((response) => {
+      const inSet = response.data;
+      console.log(inSet);
     })
     .catch((error) => {
-        console.error("Error fetching information:", error);
-        throw error;
+      console.error("Error fetching information:", error);
+      throw error;
     });
-}
+};
 
 // @PutMapping("/{userId}/{teamId}/addUser")
 // public ResponseEntity<String> addUser
-const addUser = (userId, teamId) => {
-    return axios.put
-    (API_URL + `team/${userId}/${teamId}/addUse`, {
-        userId, teamId
-    }, 
-        {withCredentials: true,}
+const addUserToTeam = (userId, teamId) => {
+  return axios
+    .put(
+      API_URL + `team/${userId}/${teamId}/addUser`,
+      {
+        userId,
+        teamId,
+      },
+      { withCredentials: true }
     )
     .then((response) => {
-        const inSet = response.data;
-        console.log(inSet)
-        return inSet;
+      const inSet = response.data;
+      console.log(inSet);
+      return inSet;
     })
     .catch((error) => {
-        console.error("Error fetching information:", error);
-        throw error;
+      console.error("Error fetching information:", error);
+      throw error;
     });
-}
+};
 
 // @DeleteMapping("/{teamId}/deleteTeamById")
 // public ResponseEntity<String> deleteTeamById
 const deleteTeamById = (teamId) => {
-    return axios.delete 
-    (API_URL +`team/${teamId}/deleteTeamById`, {
-        teamId
-    }, 
-        { withCredentials: true,}
-    );
-} 
+  return axios.delete(
+    API_URL + `team/${teamId}/deleteTeamById`,
+    {
+      teamId,
+    },
+    { withCredentials: true }
+  );
+};
 
 // @GetMapping("/{teamId}/getTeamById")
 // public ResponseEntity<Team> getTeamById
 const getTeamById = (teamId) => {
-    return axios.get
-    (API_URL + `team/${teamId}/getTeamById`, {
-        teamId
-    }, 
-        { withCredentials: true,}
+  return axios
+    .get(
+      API_URL + `team/${teamId}/getTeamById`,
+      {
+        teamId,
+      },
+      { withCredentials: true }
     )
-    .then((response) => { 
-        const teamId = response.data;
-        return teamId;
+    .then((response) => {
+      const teamId = response.data;
+      return teamId;
     })
     .catch((error) => {
-        console.error("Error fetching information:", error);
-        throw error;
+      console.error("Error fetching information:", error);
+      throw error;
     });
-}
+};
 
 // @GetMapping("/{teamId}/getAllMeetings")
 // public ResponseEntity<?> getAllMeetingsInTeam
 const getAllMeetingsInTeam = (teamId) => {
-    return axios.get
-    (API_URL + `team/${teamId}/getAllMeetingsInTeam`, {
-        teamId
-    }, 
-        { withCredentials: true,}
+  return axios
+    .get(
+      API_URL + `team/${teamId}/getAllMeetingsInTeam`,
+      {
+        teamId,
+      },
+      { withCredentials: true }
     )
-    .then((response) => { 
-        const meetingsInTeam = response.data;
-        return meetingsInTeam;
+    .then((response) => {
+      const meetingsInTeam = response.data;
+      return meetingsInTeam;
     })
     .catch((error) => {
-        console.error("Error fetching information:", error);
-        throw error;
+      console.error("Error fetching information:", error);
+      throw error;
     });
-}
+};
 
 const connectGoogleCalendar = (userId) => {
-    return axios.get
-    (API_URL + `Google/${userId}/startOAuth`, {
-        userId
-    },)
+  return axios
+    .get(API_URL + `Google/${userId}/startOAuth`, {
+      userId,
+    })
     .then((response) => {
-        const link = response.data;
-        return link;
+      const link = response.data;
+      return link;
     })
     .catch((error) => {
-        console.error("Error getting google link: ", error)
-        throw error;
-    })
-    
-}
+      console.error("Error getting google link: ", error);
+      throw error;
+    });
+};
 
 const Service = {
-    createTeam,
-    addUser,
-    deleteTeamById,
-    getTeamById,
-    getAllMeetingsInTeam,
-    connectGoogleCalendar
-}
+  createTeam,
+  addUserToTeam,
+  deleteTeamById,
+  getTeamById,
+  getAllMeetingsInTeam,
+  connectGoogleCalendar,
+  signUp,
+  login,
+};
 
 export default Service;
