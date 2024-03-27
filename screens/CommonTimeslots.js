d2dd377692c997288e6ec2709008f4ba6dc7a89c
Chen Hua-En <chenhuaen171@Chens-MacBook-Pro.local>
2024-03-27 21:46:14 +0800

    changes

--- screens/CommonTimeslots.js
+++ screens/CommonTimeslots.js
@@ -178,7 +178,7 @@ const CommonTimeslots = ({ navigation, route }) => {
           style={styles.button}
           onPress={handleUserEventsClick}
         >
-          My Calendar
+          My Calendar Within Meeting Time Frame
         </Button>
 
         <Button
